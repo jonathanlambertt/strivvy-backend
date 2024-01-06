947ceab36ccae93b8efacb195d1bf03b7a2457fb
jonathan <jonathanlambert247@gmail.com>
2024-01-06 11:57:25 -0800

    set up for linux dev env

--- strivvy/settings.py
+++ strivvy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.19', '192.168.1.19:8000']
+ALLOWED_HOSTS = ['192.168.1.19', '192.168.1.19:8000', '192.168.1.153', '192.168.1.153:8000']
 
 
 # Application definition
