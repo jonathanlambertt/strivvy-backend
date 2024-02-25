ca1b72a51f5c93b2d1b4e6414aee38da3723c45f
jonathan <jonathanlambert247@gmail.com>
2024-02-24 16:10:24 -0800

    fix post date field

--- posts/models.py
+++ posts/models.py
@@ -1,13 +1,16 @@
 import uuid
-from datetime import datetime
+from django.utils import timezone
 from django.db import models
 from users.models import User
+
+def get_current_time():
+    return timezone.now()
 
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     url = models.URLField()
-    date = models.DateTimeField(default=datetime.now)
+    date = models.DateTimeField(default=get_current_time)
     description = models.CharField(max_length=255, blank=True, null=True, default="null")
     title = models.CharField(max_length=255, blank=True, null=True, default="null")
     thumbnail = models.URLField(max_length=500, blank=True, null=True, default="null")
